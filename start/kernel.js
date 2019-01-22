62a398410ca795079e715d3ed94db3f3d73898b6
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2019-01-22 14:35:28 -0300

    Cria a parte de convite para equipe e envio de e-mail para usuários que ainda não estão cadastrados na plataforma

--- start/kernel.js
+++ start/kernel.js
@@ -12,9 +12,7 @@ const Server = use('Server')
 | match.
 |
 */
-const globalMiddleware = [
-  'Adonis/Middleware/BodyParser'
-]
+const globalMiddleware = ['Adonis/Middleware/BodyParser']
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +33,8 @@ const globalMiddleware = [
 */
 const namedMiddleware = {
   auth: 'Adonis/Middleware/Auth',
-  guest: 'Adonis/Middleware/AllowGuestOnly'
+  guest: 'Adonis/Middleware/AllowGuestOnly',
+  team: 'App/Middleware/Team'
 }
 
 /*
@@ -53,7 +52,6 @@ const serverMiddleware = [
   'Adonis/Middleware/Cors'
 ]
 
-Server
-  .registerGlobal(globalMiddleware)
+Server.registerGlobal(globalMiddleware)
   .registerNamed(namedMiddleware)
   .use(serverMiddleware)
